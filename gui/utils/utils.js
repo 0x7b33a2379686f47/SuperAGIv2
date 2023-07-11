b9dea2d6955e646192b875f28f0ce85abac88a7b
NishantBorthakur <nist.16br@gmail.com>
2023-07-11 12:34:06 +0530

    minor fix

--- gui/utils/utils.js
+++ gui/utils/utils.js
@@ -280,6 +280,9 @@ export const resetLocalStorage = (contentType, internalId) => {
     case 'Knowledge':
       removeKnowledgeInternalId(internalId);
       break;
+    case 'Add_Knowledge':
+      removeKnowledgeInternalId(internalId);
+      break;
     case 'Add_Database':
       removeAddDatabaseInternalId(internalId);
       break;
@@ -354,7 +357,7 @@ export const returnDatabaseIcon = (database) => {
   return dbTypeIcons[database]
 };
 
-export const  convertToTitleCase = (str) => {
+export const convertToTitleCase = (str) => {
   const words = str.toLowerCase().split('_');
   const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
   return capitalizedWords.join(' ');
